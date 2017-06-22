e4d75e17fff9cccb5f744807bf1042c5683e904f
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-06-22 15:16:31 -0400

    Create function with specific parameters and manipulated them in the Main function

--- gpa-calculator.py
+++ gpa-calculator.py
@@ -1,16 +1,12 @@
 # Creating a gpa calculator
 # need variables for each subject
+# need to calculate average
 # if and elif statements 
-# --> if variable is between 85-100, then gpa is 4.0
+# --> i.e. if variable is between 85-100, then gpa is 4.0
 # then return overall average
 
-def main():
-	subject1 = 94
-	subject2 = 88
-	subject3 = 89
-	subject4 = 89
-	subject5 = 85
-
+def subject_grades(subject1, subject2, subject3, subject4, subject5):
+	
 	average_subject = (subject1 + subject2 + subject3 + subject4 + subject5)/5
 	print(average_subject)
 
@@ -20,9 +16,9 @@ def main():
 		print("Your GPA is 3.7")
 	elif average_subject > 75:
 		print("Your GPA is 3.3")
-	elif average_subject > 70:
+	elif average_subject > 69:
 		print("Your GPA is 3.0")
-	elif average_subject > 60:
+	elif average_subject > 59:
 		print("Your GPA is 2.3")
 	elif average_subject > 55:
 		print("Your GPA is 1.7")
@@ -30,6 +26,9 @@ def main():
 		print("Your GPA is 1.0")
 	else:
 		print("I'm sorry, you have failed the semester. Your GPA is 0")
+
+def main():
+	subject_grades(67, 70, 89, 50, 76)
 
 if __name__ == '__main__':
 	main()
