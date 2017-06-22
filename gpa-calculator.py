4d883f5bb3f846705b5d6a7db96598c6020a8319
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-06-22 15:02:37 -0400

    Add varaibles for subjects and calculate the average

---
+++ gpa-calculator.py
@@ -0,0 +1,17 @@
+# Creating a gpa calculator
+# need variables for each subject
+# if and elif statements 
+# --> if variable is between 85-100, then gpa is 4.0
+# then return overall average
+
+def main():
+		subject1 = 75
+		subject2 = 88
+		subject3 = 66
+		subject4 = 82
+		subject5 = 85
+
+		average_subject = (subject1 + subject2 + subject3 + subject4 + subject5)/5
+
+if __name__ == '__main__':
+	main()
